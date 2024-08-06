ebccab81b6fcc26e9b9ae4539c4b58c7a15f42fc
dah <cdh010126r@gmail.com>
2024-08-07 02:54:25 +0900

    feat. mock 리뷰 등록

---
+++ src/mock/review.ts
@@ -0,0 +1,53 @@
+import { http, HttpResponse } from 'msw';
+import { BookReviewItem } from '../models/book.model';
+import { fakerKO as faker } from '@faker-js/faker';
+
+// const mockReviewData: BookReviewItem[] = [
+//   {
+//     id: 1,
+//     userName: 'dah',
+//     content: '안녕하세요',
+//     createAt: '2024-01-01',
+//     score: 5,
+//   },
+//   {
+//     id: 2,
+//     userName: 'datdaradanadat',
+//     content: '감사합니다',
+//     createAt: '2024-08-01',
+//     score: 3,
+//   },
+// ];
+
+const mockReviewData: BookReviewItem[] = Array.from({ length: 8 }).map(
+  (_, index) => ({
+    id: index,
+    userName: `${faker.person.lastName()}${faker.person.firstName()}`,
+    content: faker.lorem.paragraph(),
+    createAt: faker.date.past().toISOString(),
+    score: faker.helpers.rangeToNumber({ min: 1, max: 5 }),
+  }),
+);
+
+export const reviewsById = http.get(
+  'http://localhost:2222/reviews/:bookId',
+  () => {
+    return HttpResponse.json(mockReviewData, {
+      status: 200,
+    });
+  },
+);
+
+export const addReview = http.post(
+  'http://localhost:2222/reviews/:bookId',
+  () => {
+    return HttpResponse.json(
+      {
+        message: '리뷰가 등록되었습니다',
+      },
+      {
+        status: 200,
+      },
+    );
+  },
+);
