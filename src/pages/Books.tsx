7ca3098e6bc7322780cbc3b545f5012d3b90383f
dah <cdh010126r@gmail.com>
2024-08-08 01:27:21 +0900

    feat. intersectionObserver 무한스크롤

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -3,14 +3,34 @@ import Title from '../components/common/Title';
 import BooksFilter from '../components/books/BooksFilter';
 import BooksList from '../components/books/BooksList';
 import BooksEmpty from '../components/books/BooksEmpty';
-import Pagination from '../components/books/Pagination';
 import BooksViewSwitcher from '../components/books/BooksViewSwitcher';
-import { useBooks } from '../hooks/useBooks';
 import styled from 'styled-components';
 import Loading from '../components/common/Loading';
+import { useBooksInfinite } from '../hooks/useBooksInfinite';
+import Button from '../components/common/Button';
+import { useIntersectionObserver } from '../hooks/useIntersectionObserver';
+// import { useBooks } from '../hooks/useBooks'
 
 const Books = () => {
-  const { books, pagination, isEmpty, isBooksLoading } = useBooks();
+  const {
+    books,
+    pagination,
+    isEmpty,
+    isBooksLoading,
+    fetchNextPage,
+    hasNextPage,
+  } = useBooksInfinite();
+
+  const moreRef = useIntersectionObserver(([entry]) => {
+    if (entry.isIntersecting) {
+      loadMore();
+    }
+  });
+
+  const loadMore = () => {
+    if (!hasNextPage) return;
+    fetchNextPage();
+  };
 
   if (isEmpty) return <BooksEmpty />;
 
@@ -25,7 +45,17 @@ const Books = () => {
           <BooksViewSwitcher />
         </div>
         <BooksList books={books} />
-        <Pagination pagination={pagination} />
+        {/* <Pagination pagination={pagination} /> */}
+        <div className="more" ref={moreRef}>
+          <Button
+            size="medium"
+            scheme="normal"
+            onClick={() => fetchNextPage()}
+            disabled={!hasNextPage}
+          >
+            {hasNextPage ? '더보기' : '마지막 페이지'}
+          </Button>
+        </div>
       </BookStyle>
     </>
   );
